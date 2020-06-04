c9748c23e87f251b3e2784b38b36afbc5e5bfdfe
Calista Ortiz Quirno <calistaortiz@MacBook-Pro-de-Calista.local>
2020-06-04 18:09:33 -0300

    changes

--- js/script.js
+++ js/script.js
@@ -6,25 +6,23 @@ fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart")
 })
 .then(function (resultado) {
   console.log(resultado);
-})
+//canciones
 var listaCanciones = document.querySelector("#tracks");
 var contenidoCanciones = "";
 for (let i = 0; i < resultado.tracks.data.length; i++) {
   var canciones1 = resultado.tracks.data[i];
-  contenidoCanciones += '<div class="cajas" id="tracks"> ' + ' <ul>';
-  contenidoCanciones += ' <li class="Tracks"><a href="albums.html">Tracks</a></li>' + '</ul>';
-} }
+  contenidoCanciones += '<button type="button" class="list-group-item list-group-item-action text-black">'  + '<a href="tracks.html">' + canciones1.title + '</a>' + '</button>'
+} 
+document.querySelector("#imagen").innerHTML='<img src="'+ resultado.tracks.data[1].album.cover+'" class="rounded img-fluid "alt="...">'
 
 listaCanciones.innerHTML= contenidoCanciones
 Console.log(listaCanciones);
-})
+//albums
+});
+};
 
 
-  //<div class="cajas" id="tracks"> 
-        
-  //      <ul>
-  //           <li class="Tracks"><a href="albums.html">Tracks</a></li>
-  //      </ul>
+
   
   
   
@@ -34,118 +32,7 @@ Console.log(listaCanciones);
   
   
   
-  
-  
-//  window.onload=function(){
-// fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/302127')
-// .then(function(response) {
-  // return response.json()
-// })
-// .then(function(resultado) {
-  // console.log(resultado);
-  /*
-  var imagen=resultado.data.image_url;
 
-  var div=document.querySelector(".gifRandom")
-  div.innerHTML='<img alt="cualquier gif"src="'+imagen+'">';
-  */
-// })
-// .catch(function(error) {
-  // console.log("Error: " + error);
-// })
-
-//} //no borrar jamas
-
-// window.onload=function(){
-// fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart')
-//  .then(function(response) {
-//   return response.json()
-//  })
-// .then(function(myJson) {
-//  console.log(myJson);
-
- 
-//  var carrousel = document.querySelector('#ArtistasCarr');
-//  var Artista="";
-//  for (let i = 0; i <myJson.artists.data.length; i++) {
-//    const element = myJson.artists.data[i];
-//   <div class="uk-card-media-top">';
-//    Artista=+'<img src="' + element.picture_big+'" alt="">' +'</div>'
-//    Artista+= '<div class="uk-card-body"' + '<h3 class="uk-card-title">'+ element.name+'</h3>'
-//    Artista += '<p>'+element.followers+'</p>'+'</div>'+'</div>' +'</li>'
-   
-//  }
-//  carrousel.innerHTML = Artista
-//  console.log(carrousel);
- 
-// })
-//  .catch(function(error) {
-//     console.log("Error: " + error);
-//    })
-//   }
-
- //cajas home
-//  window.onload=function(){
-//  fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/')
-   
-//  .then(function(response) {
-//     return response.json()
-//    })
-//  .then(function(myJson) {
-//    console.log(myJson);
-//  }
-  
-   
-  //  var cajas = document.querySelector('#tracks');
-  //  var tracks="";
-  //  for (let i = 0; i <myJson.tracks.data.length; i++) {
-  //     const element = myJson.tracks.data[i];
-  //  }
-  //  cajas.innerHTML= tracks
-  //  console.log(cajas);
-  // })
-  //   .catch(function(error) {
-  //        console.log("Error: " + error);
-  //   })
-         
- 
-     // tracks =+ "<li>" +'<div class="uk-card uk-card-default">'+'<div class="uk-card-media-top">';
-     // tracks=+'<img src="' + element.picture_big+'" alt="">' +'</div>'
-     // tracks+= '<div class="uk-card-body"' + '<h3 class="uk-card-title">'+ element.name+'</h3>'
-     // tracks+= '<p>'+element.followers+'</p>'+'</div>'+'</div>' +'</li>'
-
-   
-   
-
-
-
-//  window.onload=function(){
-//     fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/tracks')
-//      .then(function(response) {
-//       return response.json()
-//      })
-//     .then(function(myJson) {
-//      console.log(myJson);
-
-//      var cajas= document.querySelector('tracks');
-//      var 
-
-
-//      <div class="cajas" id="tracks"> 
-        
-//      <ul>
-//           <li class="Tracks"><a href="albums.html">Tracks</a></li>
-//      </ul>
-// <div class="cajas"id="Albums">
-//      <ul>
-//           <li class="Albums"><a href="albums.html">Albums</a></li>
-//       </ul>
-//   </div>
-// <div class="cajas" id="Artistas">
-//      <ul><li class="Artistas"><a href="albums.html">Artistas</a></li>
-//      </ul>
-         
-//  </div> 
 
 
 
