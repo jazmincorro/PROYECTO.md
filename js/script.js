94433e5f4d119e09d55ce9240e14fc3eaaeaadc9
Calista Ortiz Quirno <calistaortiz@MacBook-Pro-de-Calista.local>
2020-06-04 12:38:01 -0300

    no more carrousel

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
  
 
+fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart")
+.then(function (response) {
+  return response.json();
+})
+.then(function (resultado) {
+  console.log(resultado);
+  var listaCanciones = document.querySelector("#listaCanciones");
+  var contenidoCanciones = "";
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 //  window.onload=function(){
 // fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/302127')
 // .then(function(response) {
@@ -20,29 +39,100 @@
 
 //} //no borrar jamas
 
-window.onload=function(){
-fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/3135556')
- .then(function(response) {
-  return response.json()
- })
-.then(function(myJson) {
- console.log(myJson);
-
- var title= myJson.title;
- var carrousel = document.querySelector('#carrousel');
- carrousel.innerHTML+= '<h3 class="uk-card-title">'+title+'</h3>'
- 
- var picture= myJson.picture;
- var foto= document.querySelector('#foto');
- foto.innerHTML+='<img src="images/photo.jpg"> '+picture+' <alt="">'
-
+// window.onload=function(){
+// fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart')
+//  .then(function(response) {
+//   return response.json()
+//  })
+// .then(function(myJson) {
+//  console.log(myJson);
 
  
- })
- .catch(function(error) {
-    console.log("Error: " + error);
-   })
-  }
+//  var carrousel = document.querySelector('#ArtistasCarr');
+//  var Artista="";
+//  for (let i = 0; i <myJson.artists.data.length; i++) {
+//    const element = myJson.artists.data[i];
+//    Artista =+ "<li>" +'<div class="uk-card uk-card-default">'+'<div class="uk-card-media-top">';
+//    Artista=+'<img src="' + element.picture_big+'" alt="">' +'</div>'
+//    Artista+= '<div class="uk-card-body"' + '<h3 class="uk-card-title">'+ element.name+'</h3>'
+//    Artista += '<p>'+element.followers+'</p>'+'</div>'+'</div>' +'</li>'
+   
+//  }
+//  carrousel.innerHTML = Artista
+//  console.log(carrousel);
+ 
+// })
+//  .catch(function(error) {
+//     console.log("Error: " + error);
+//    })
+//   }
+
+ //cajas home
+//  window.onload=function(){
+//  fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/')
+   
+//  .then(function(response) {
+//     return response.json()
+//    })
+//  .then(function(myJson) {
+//    console.log(myJson);
+//  }
+  
+   
+  //  var cajas = document.querySelector('#tracks');
+  //  var tracks="";
+  //  for (let i = 0; i <myJson.tracks.data.length; i++) {
+  //     const element = myJson.tracks.data[i];
+  //  }
+  //  cajas.innerHTML= tracks
+  //  console.log(cajas);
+  // })
+  //   .catch(function(error) {
+  //        console.log("Error: " + error);
+  //   })
+         
+ 
+     // tracks =+ "<li>" +'<div class="uk-card uk-card-default">'+'<div class="uk-card-media-top">';
+     // tracks=+'<img src="' + element.picture_big+'" alt="">' +'</div>'
+     // tracks+= '<div class="uk-card-body"' + '<h3 class="uk-card-title">'+ element.name+'</h3>'
+     // tracks+= '<p>'+element.followers+'</p>'+'</div>'+'</div>' +'</li>'
+
+   
+   
+
+
+
+//  window.onload=function(){
+//     fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/tracks')
+//      .then(function(response) {
+//       return response.json()
+//      })
+//     .then(function(myJson) {
+//      console.log(myJson);
+
+//      var cajas= document.querySelector('tracks');
+//      var 
+
+
+//      <div class="cajas" id="tracks"> 
+        
+//      <ul>
+//           <li class="Tracks"><a href="albums.html">Tracks</a></li>
+//      </ul>
+// <div class="cajas"id="Albums">
+//      <ul>
+//           <li class="Albums"><a href="albums.html">Albums</a></li>
+//       </ul>
+//   </div>
+// <div class="cajas" id="Artistas">
+//      <ul><li class="Artistas"><a href="albums.html">Artistas</a></li>
+//      </ul>
+         
+//  </div> 
+
+
+
+
   
 
 
