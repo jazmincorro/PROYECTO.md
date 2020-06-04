c02d4c40de0e767a220f3f6f321b31a86937407e
jazmincorro <63121591+jazmincorro@users.noreply.github.com>
2020-06-04 00:03:30 -0300

    javascript api

--- js/script.js
+++ js/script.js
@@ -1,41 +1,51 @@
- window.onload=function(){
-fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/302127')
-.then(function(response) {
-  return response.json()
-})
-.then(function(resultado) {
-  console.log(resultado);
+ 
+
+//  window.onload=function(){
+// fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/302127')
+// .then(function(response) {
+  // return response.json()
+// })
+// .then(function(resultado) {
+  // console.log(resultado);
   /*
   var imagen=resultado.data.image_url;
 
   var div=document.querySelector(".gifRandom")
   div.innerHTML='<img alt="cualquier gif"src="'+imagen+'">';
   */
-})
-.catch(function(error) {
-  console.log("Error: " + error);
-})
+// })
+// .catch(function(error) {
+  // console.log("Error: " + error);
+// })
 
+//} //no borrar jamas
 
-
-} //no borrar jamas
-
+window.onload=function(){
 fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/3135556')
-.then(function(response) {
+ .then(function(response) {
   return response.json()
-})
+ })
 .then(function(myJson) {
-  console.log(myJson);
-})
-.catch(function(error) {
+ console.log(myJson);
+
+ var title= myJson.title;
+ var carrousel = document.querySelector('#carrousel');
+ carrousel.innerHTML+= '<h3 class="uk-card-title">'+title+'</h3>'
+ 
+ var picture= myJson.picture;
+ var foto= document.querySelector('#foto');
+ foto.innerHTML+='<img src="images/photo.jpg"> '+picture+' <alt="">'
+
+
+ })
+ .catch(function(error) {
     console.log("Error: " + error);
-  })
-//var listaTracks=resultado.nb_tracks;
-//var listaTracks=document.querySelector('.listaTracks');
-//var listaTracks= nb_tracks;
-
-
-
-
-
+   })
+  }
   
+
+
+
+
+
+
