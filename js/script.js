1ba2ee1aaf1770cdc304a684887d0a45c49fb732
Calista Ortiz Quirno <calistaortiz@MacBook-Pro-de-Calista.local>
2020-06-05 18:02:48 -0300

    home listo

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ window.onload=function(){
     
     }
 
-      document.querySelector("#imagen").innerHTML='<img src="'+ resultado.tracks.data[1].album.cover+'" class="rounded img-fluid "alt="...">'
+      document.querySelector("#imagen").innerHTML='<img src="'+ resultado.tracks.data[1].album.cover_xl+'" class="rounded img-fluid "alt="...">'
 
     listaCanciones.innerHTML= contenidoCanciones
     console.log(listaCanciones);
@@ -28,7 +28,7 @@ window.onload=function(){
        var album1 = resultado.albums.data[i];
        contenidoAlbums += '<button type="button" class="list-group-item list-group-item-action text-black">' + '<a href="albums.html">' + album1.title + '</a>'+'</button>'
      } 
-     document.querySelector("#imagenAlbums").innerHTML='<img src="'+ resultado.albums.data[1].cover+'"  class="rounded img-fluid "alt="...">'
+     document.querySelector("#imagenAlbums").innerHTML='<img src="'+ resultado.albums.data[1].cover_xl+'"  class="rounded img-fluid "alt="...">'
 
      listaAlbums.innerHTML= contenidoAlbums
      console.log(listaAlbums);
@@ -41,7 +41,7 @@ window.onload=function(){
        var Artist1 = resultado.artists.data[i];
        contenidoArtistas += '<button type="button" class="list-group-item list-group-item-action text-black">' + '<a href="artistas.html">' + Artist1.name + '</a>'+'</button>'
      } 
-     document.querySelector("#imagenArtista").innerHTML='<img src="'+ resultado.artists.data[1].cover+'"  class="rounded img-fluid "alt="...">'
+     document.querySelector("#imagenArtista").innerHTML='<img src="'+ resultado.artists.data[1].picture_xl+'"  class="rounded img-fluid "alt="...">'
 
      listaArtistas.innerHTML= contenidoArtistas
      console.log(listaArtistas);
