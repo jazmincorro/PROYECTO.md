46407e94f85918bc7a245a119f9fa1a9ea83101c
jazmincorro <63121591+jazmincorro@users.noreply.github.com>
2020-06-12 00:00:46 -0300

    linkeado album en track

--- js/scripttracks.js
+++ js/scripttracks.js
@@ -25,7 +25,7 @@ window.onload=function(){
           </div>
           <div data-spy="scroll" data-target="#list-example" data-offset="0" class="scrollspy-example"></div> 
 
-         <a href="artistas.html?id=${resultado.album.id}"><h1>Album: ${resultado.album.title} </h1> </a>
+         <a href="albums.html?id=${resultado.album.id}"><h1>Album: ${resultado.album.title} </h1> </a>
 
          <a href="artistas.html?id=${resultado.artist.id}"><h1>Artist: ${resultado.artist.name} </h1> </a>
              <div class="cajas d-flex" id="Tracks-nousar"> 
@@ -46,42 +46,3 @@ window.onload=function(){
         });
     };
 
-
-
-
-// // window.onload=function(){
-// //     var querystring= location.search;
-// //     var querystringObj= new URLSearchParams (querystring);
-// //     var LoQueBuscoElUsuario= querystringObj.get('textoBuscado');
-// //     // fijarse de ver si el fetch necesita modificarse
-// //     fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/3135556')
-// //     .then(function (response) {
-// //         return response.json();
-// //     })
-// //     .then(function (resultado) {
-// //         console.log(resultado);
-
-// //         var listaAll=document.querySelector("#Album");
-// //         var contenidoAlbumes= "";
-// //      for  (let i = 0; i < resultado.album.length; i++) {
-// //          var albumes= resultado.album.data[i];
-// //          contenidoAlbumes+=  + '<a href="albums.html">' + album.title + '</a>'
-// //          contenidoAlbumes+= + '<h1> ' + album.cover_xl+ '</h1>' //chequear linea falta
-// //      }
-// //      document.querySelector("#imagenAlbums").innerHTML='<img src="'+ resultado.album.cover_xl+'"  class="rounded img-fluid "alt="...">'
-// //      listaAll.innerHTML= contenidoAlbumes
-// //      console.log(listaAll);
-// //          //   } '
-// //     //  var listaAlbums = document.querySelector("#Album");
-//     //  var contenidoAlbums = "";
-//     //  for (let i = 0; i < resultado.albums.data.length; i++) {
-//     //    var album1 = resultado.albums.data[i];
-//     //    contenidoAlbums += '<button type="button" class="list-group-item list-group-item-action text-black">' + '<a href="albums.html">' + album1.title + '</a>'+'</button>'
-//     //  } 
-//      // document.querySelector("#imagenAlbums").innerHTML='<img src="'+ resultado.albums.data[1].cover_xl+'"  class="rounded img-fluid "alt="...">'
-
-//      //listaAlbums.innerHTML= contenidoAlbums
-//      //console.log(listaAlbums);
-    
-// });
-// }
