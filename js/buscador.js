649d8ae38945a5ce5846465ccffb66e54b00ec3b
jazmincorro <63121591+jazmincorro@users.noreply.github.com>
2020-06-19 00:00:03 -0300

    ultimo

--- js/buscador.js
+++ js/buscador.js
@@ -1,36 +1,34 @@
 window.onload = function () {
+
     var queryString= location.search;
-    var queryStringObj= new this.URLSearchParams(queryString);
-    var Busqueda= queryStringObj.get ('q');
+    var queryStringObj= new URLSearchParams(queryString);
+    var busqueda= queryStringObj.get ('id');
 
-    fetch('"https://cors-anywhere.herokuapp.com/https://api.deezer.com/track?q='+ Busqueda)
-
-
+    fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q='+ busqueda)
     .then(function (response) {
         return response.json();
     })
-    .then(function (resultado) {
-        console.log(resultado.data);
+    .then(function (data) {
+        console.log(data);
     
-    var ContenidoTracks= ""
-    
-    for (let i = 0; i < resultado.data.length; i++) {
-        const element = resultado.data[i];
-        console.log(element)
-        var Cancion= element.title 
-        var Duracion= element.duration
-        var Albumfoto= element.album.cover
-        var Albumtitle= element.album.title
-        var Artista= element.artist.name
-        
-
+        var listaCanciones = document.querySelector("#Tracks");
+        var contenidoCanciones = "";
+        for (let i = 0; i < data.data.length; i++) {
+          var canciones1 = data.data[i];
+          contenidoCanciones += '<button type="button" class="list-group-item list-group-item-action">' 
+          contenidoCanciones += '<a href="tracks.html?id=' +
+          canciones1.id + '" class="text-blackt">' + 
+       
+          canciones1.title + '</a>' + '</button>'
+        }
+{/* 
         ContenidoTracks+=  
         `<form class="form-inline my-2 my-lg-0" action <a href="buscador.html?id=${element.title}" method="GET"></form> 
-        <input class="form-control mr-sm-2" type="search" name="q" placeholder="SEARCH">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        <input class="form-control mr-sm-2" type="search" name="id" placeholder="SEARCH">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit"> </button>
               </form> `
-        
+         */}
     
 });
-       
-}
+
+    }
