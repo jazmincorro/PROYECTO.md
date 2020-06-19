dee8a6d964d845902a7cacf542dd95d237836468
Tomas Rotholtz <tomas@MacBook-Air.local>
2020-06-18 21:19:24 -0300

    Resultado busqueda de generos

--- js/resultadobusquedageneros.js
+++ js/resultadobusquedageneros.js
@@ -1,35 +1,68 @@
 window.onload=function(){
     var queryStringD =document.location.search;
     var queryStringObj = new URLSearchParams (queryStringD);
-    var id= queryStringObj.get("id");
-    fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/" + id)
-    var ImagenGeneroD= resultado.picture;
-    var IdGeneroD= resultado.id;
-    var ImagenGen= documnt.querySelector( '#ImagenGenero')
-    var  Name= document.querySelector('#NombreG')
-    ImagenGen.innerHTML += "<img src= '"+ ImagenGeneroD+"' alt'>";
-   
-   
+    var idQuerySelector= queryStringObj.get("id");
+  console.log(idQuerySelector)
+   // var typeQuerySelector= queryStringObj.get('type')
+    fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/" + idQuerySelector)
+
     .then(function (response) {
         return response.json();
     })
     .then(function (resultado) {
-        console.log(resultado);
-   var queryStringG =document.location.search;
-    var queryStringObj = new URLSearchParams (queryStringG);
-    var idGeneros= queryStringObj.get("id");
+    
+// if (typeQuerySelector== track){
+
+// var tracks= document.querySelector('.info-songs')
+// tracks.style.display= 'flex'
+// var trackTitle= resultado.title;
+// var trackAlbum = resultado.album.title;
+
+
+    var NombreGenD= resultado.Name;
+    var ImagenGeneroD= resultado.picture;
+    var IdGeneroD= resultado.id;
+    var ImagenGen= document.querySelector( '#ImagenGenero')
+    var  Name= document.querySelector('#NombreG')
+
+    ImagenGen.innerHTML += "<img src= '"+ ImagenGeneroD+"' alt'>";
+    Name.innerHTML+= NombreGenD
+
+   // console.log(resultado.data)
+    
    
-     fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/" + id+ "/artists")
-         
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (resultado) {
+//         console.log(resultado);
+//    var queryStringG =document.location.search;
+//     var queryStringObj = new URLSearchParams (queryStringG);
+//     var idGeneros= queryStringObj.get("id");
+// var queryStringD =document.location.search;
+// var queryStringObj = new URLSearchParams (queryStringD);
+// var IdGeneroD= queryStringObj.get("id");
+   
+     fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/" +  IdGeneroD  + "/artists")
+    
         .then(function (response) {
             return response.json();
         })
         .then(function (resultado) {
-            console.log(resultado);
-            var ContnedorGenerosD=  document.querySelector('#listaArtistas');
-
-        //     var ContenidoGenerosD='';
-        //   for (let i = 0; i < '10'; i++) {
+            console.log(resultado)
+           
+  
+           for (let i = 0; i < '10'; i++) {
+               var NombreA= resultado.data[i].Name
+               var GeneroAimagen= resultado.data[i].picture
+               var GeneroAname= document.querySelector('#listaArtistas')
+               var ArtId= resultado.data[i].id
+               GeneroAname.innerHTML+=  '<img alt="" src="'+ GeneroAimagen +'">' + NombreA +  ArtId}
+           })
+           .catch(function(error){
+            console.log("Error:"+ error);
+        });
+       
         //   const element = resultado.data[i];
     
 
@@ -53,7 +86,6 @@ window.onload=function(){
 
 })
 }// no borrar
-
 
 
 
